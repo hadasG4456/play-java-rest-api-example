05615a2198b5a384553fedc35113cca93152368f
hadasG4456 <srsxh,!995>
2022-01-10 12:54:26 +0200

    Changes by tomew

--- app/models/PeopleModel.java
+++ app/models/PeopleModel.java
@@ -1,12 +1,29 @@
 package models;
 
+import org.springframework.ui.Model;
+
 import java.sql.*;
-public class PeopleModel {
+
+@javax.persistence.Entity
+public class PeopleModel{
+
     public Connection conn;
     public PeopleModel() throws SQLException {
         this.conn = DriverManager.
-                getConnection("jdbc:h2:./app/DB/MangerDB", "sa", "");
-        // add application code here
+                getConnection("jdbc:h2:./app/DB/MangerDB", "sa", "sa");
+        Statement stmt = conn.createStatement();
+        String sql = "CREATE TABLE PEOPLE " +
+                "(id VARCHAR not NULL, " +
+                " first VARCHAR(255), " +
+                " last VARCHAR(255), " +
+                " age INTEGER, " +
+                " PRIMARY KEY ( id ))";
+
+        stmt.executeUpdate(sql);
+
+    }
+    // close the connection
+    public void closeCon() throws SQLException {
         conn.close();
     }
 }
